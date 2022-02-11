1ad748a96af376ef931344e879ef94d66bdf23ad
phasetr <phasetr@gmail.com>
2022-02-11 20:41:13 +0900

    Add a solution for DDCC2020 B in AtCoder

---
+++ AtCoder/DDCC2020/B1.fsx
@@ -0,0 +1,15 @@
+﻿@"https://atcoder.jp/contests/ddcc2020-qual/tasks/ddcc2020_qual_b"
+#r "nuget: FsUnit"
+open FsUnit
+
+let solve N As =
+    let s = Array.sum As
+    ((0L, s),As) ||> Array.scan (fun (accl, accr) a -> (accl+a, accr-a))
+    |> Array.fold (fun acc (l,r) -> min acc (abs (l-r))) s
+
+let N = stdin.ReadLine() |> int
+let As = stdin.ReadLine().Split() |> Array.map int64
+solve N As |> stdout.WriteLine
+
+solve 3 [|2L;4L;3L|] |> should equal 3L
+solve 12 [|100L;104L;102L;105L;103L;103L;101L;105L;104L;102L;104L;101L|] |> should equal 0L
