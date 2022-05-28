0db4786b3e890b89c1c224c9f5cd41967bc28a4f
phasetr <phasetr@gmail.com>
2022-05-28 21:13:54 +0900

    Update

--- AOJ/ALDS1/06D01_00.hs
+++ AOJ/ALDS1/06D01_00.hs
@@ -5,7 +5,7 @@ import Data.Char (isSpace)
 import qualified Data.List as L
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as VM
-import qualified Data.IntMap.Strict as M
+import qualified Data.IntMap.Strict as IMS
 import qualified Data.ByteString.Char8 as B
 
 main :: IO ()
@@ -15,9 +15,9 @@ main = do
   print $ solve wv
 
 solve :: V.Vector Int -> Int
-solve wv = V.sum $ V.map (getCost (V.minimum wv)) cv where
+solve wv = V.sum $ V.map (getCost (V.minimum wv)) cyclv where
   fv = getFlags wv
-  cv = getCycles wv fv
+  cyclv = getCycles wv fv
 
 getFlags :: V.Vector Int -> V.Vector Bool
 getFlags wv = runST $ do
@@ -27,29 +27,29 @@ getFlags wv = runST $ do
   V.freeze fv
 
 getCycles :: V.Vector Int -> V.Vector Bool -> V.Vector (V.Vector Int)
-getCycles costs fv = V.map V.fromList $ runST $ do
+getCycles costv fv = V.map V.fromList $ runST $ do
   fmv <- V.thaw fv
-  V.mapM (getCycle costs sorted fmv []) costs
-  where sorted = M.fromList $ zip (L.sort $ V.toList costs) [0..]
+  V.mapM (getCycle costv im fmv []) costv
+  where im = IMS.fromList $ zip (L.sort $ V.toList costv) [0..]
 
-getCycle :: PrimMonad m => V.Vector Int -> M.IntMap Int -> VM.MVector (PrimState m) Bool -> [Int] -> Int -> m [Int]
-getCycle costs sorted fv cycle c = do
+getCycle :: PrimMonad m => V.Vector Int -> IMS.IntMap Int -> VM.MVector (PrimState m) Bool -> [Int] -> Int -> m [Int]
+getCycle costv im fv cycle c = do
   isValid <- VM.read fv c
   if isValid then return cycle
   else do
     VM.write fv c True
-    getCycle costs sorted fv (c:cycle) (costs V.! (sorted M.! c))
+    getCycle costv im fv (c:cycle) (costv V.! (im IMS.! c))
 
 getCost :: Int -> V.Vector Int -> Int
-getCost minV costs = case n of
+getCost minV costv = case n of
   0 -> 0
   1 -> 0
-  _ -> if minV `V.elem` costs then sum1 else min sum1 sum2
+  _ -> if minV `V.elem` costv then sum1 else min sum1 sum2
   where
-    sum1 = V.sum costs + (n - 2) * mc
-    sum2 = V.sum costs + mc + (n + 1) * minV
-    n = V.length costs
-    mc = V.minimum costs
+    sum1 = V.sum costv + (n - 2) * mc
+    sum2 = V.sum costv + mc + (n + 1) * minV
+    n = V.length costv
+    mc = V.minimum costv
 
 test = do
   print $ solve (V.fromList [1,5,3,4,2]) == 7
